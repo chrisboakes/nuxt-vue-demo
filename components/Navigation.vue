a63a55f4692279bd99e200d1d4daa1001fa91c39
Chris Boakes <chris@chrisboakes.com>
2020-04-08 13:59:01 +0100

    SiteHeader styling and markup

---
+++ components/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+	<nav>
+		<ul>
+			<li><a href='/'>Home</a></li>
+			<li><a href='/politics'>Politics</a></li>
+			<li><a href='/news'>News</a></li>
+		</ul>
+	</nav>
+</template>
+
+<style>
+	nav {
+		height: 100%;
+		grid-column: 5/12;
+	}
+
+	ul {
+		align-items: center;
+		display: flex;
+		height: 100%;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	li {
+		margin-right: 10px;
+		padding-right: 10px;
+	}
+</style>
--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,15 +1,27 @@
 <template>
-	<div>
+	<header>
 		<Logo />
-	</div>
+		<Navigation />
+	</header>
 </template>
+
+<style>
+	header {
+		border-bottom: 1px solid grey;
+		display: grid;
+		grid-template-columns: repeat(12,1fr);
+		padding: 20px;
+	}
+</style>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import Navigation from '~/components/Navigation.vue'
 
 export default {
 	components: {
-		Logo
+		Logo,
+		Navigation
 	}
 }
 </script>
